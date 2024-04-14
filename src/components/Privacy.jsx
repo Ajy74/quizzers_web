d53635679bcc859d6b4c86da02258d5179ccb64a
Ajay Mourya <mouryaajay7463@gmai.com>
2024-04-14 10:36:19 +0530

    updated privacy and terms page

--- src/components/Privacy.jsx
+++ src/components/Privacy.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import '../css/privacy.css';
 
 const Privacy = () => {
     return (
         <>
-            <h1>This is Privacy</h1>
+            <div className="privacy-container">
+                <h1 className="privacy-title">Privacy Policy</h1>
+                <div className="privacy-content">
+                    <h2>Effective Date: 12/04/2024</h2>
+
+                    <h3>Introduction:</h3>
+                    <p>Welcome to Quizzers, a quiz app designed to provide users with an engaging platform to participate in quiz contests and enhance their knowledge. This privacy policy outlines how we collect, use, and protect your personal information when you use our app.</p>
+
+                    <h3>Information We Collect:</h3>
+                    <ul>
+                        <li><span>Registration Information: </span> When you register for an account on Quizzers, we collect information such as your username, profile photo, email address (if provided), and mobile number. This information is used to create and personalize your account.</li>
+                        <li><span>KYC Information: </span> In order to participate in real quiz contests and withdraw winnings, users are required to complete a Know Your Customer (KYC) verification process. This includes providing identification documents such as Aadhar card, PAN card, voter card, or passport, as well as bank account details or UPI information for withdrawal purposes.</li>
+                        <li><span>Practice Quiz Data: </span> We collect data related to your interactions with practice quizzes, including quiz responses and performance metrics. This information is used to improve the app experience and tailor content to your preferences.</li>
+                        <li><span>Transaction Information: </span> When you add funds to your wallet or withdraw winnings, we collect transaction details such as the amount, date, and payment method used.</li>
+                        <li><span>Other Information: </span> We may also collect non-personal information such as device information, IP address, and usage data for analytics and security purposes.</li>
+                    </ul>
+
+
+                    <h3>How We Use Your Information:</h3>
+                    <ul>
+                        <li><span>To Provide Services: </span> We use the information collected to provide you with access to the features and services offered by Quizzers, including participating in quiz contests and accessing educational content.</li>
+                        <li><span>Account Management: </span>Your registration information is used to manage your account, including authentication, profile customization, and communication with you.</li>
+                        <li><span>KYC Verification: </span>KYC information is collected and verified to comply with legal requirements and ensure the security of financial transactions.</li>
+                        <li><span>Improving User Experience: </span>We analyze practice quiz data and user feedback to improve our content, features, and overall user experience.</li>
+                        <li><span>Transactional Purposes: </span>Transaction information is used for processing payments, facilitating withdrawals, and maintaining transaction records.</li>
+                    </ul>
+
+                                        
+                    <h3>Data Sharing:</h3>
+                    <p>We do not sell, rent, or share your personal information with third parties for marketing purposes. However, we may share your information with trusted third-party service providers who assist us in operating our app, conducting business, or servicing you, subject to confidentiality agreements.</p>
+
+                    <h3>Data Security:</h3>
+                    <p>We take reasonable measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction. This includes encryption, secure server hosting, and regular security audits.</p>
+
+                    <h3>User Rights:</h3>
+                    <p>You have the right to access, update, or delete your personal information at any time. You can also opt-out of receiving promotional communications from us by following the instructions provided in such communications.</p>
+
+                    <h3>Policy Updates:</h3>
+                    <p>We may update this privacy policy from time to time to reflect changes in our practices or legal requirements. We will notify you of any material changes by posting the updated policy on our website or through other communication channels.</p>
+
+                    <h3>Contact Us:</h3>
+                    <p>If you have any questions or concerns about our privacy practices or this policy, please contact us at <span className="companyEmail">quizzers14@gmail.com</span> .</p>
+
+                    <h3>Legal Compliance:</h3>
+                    <p>Quizzers complies with applicable data protection laws and regulations, including the General Data Protection Regulation (GDPR) .</p>
+                    {/* <p>Quizzers complies with applicable data protection laws and regulations, including the General Data Protection Regulation (GDPR) and the California Consumer Privacy Act (CCPA).</p> */}
+                </div>
+            </div>
         </>
     );
 }
--- src/components/TermsAndCondition.jsx
+++ src/components/TermsAndCondition.jsx
@@ -1,9 +1,54 @@
 import React from "react";
+import '../css/terms.css';
 
 const TermsAndCondition = () => {
     return (
         <>
-            <h1>This is terms and condition</h1>
+            <div className="terms-container">
+                <h1 className="terms-title">Quizzers Terms and Conditions</h1>
+                <div className="terms-content">
+                    <p>Welcome to Quizzers, an interactive quiz platform that offers users the opportunity to participate in quiz contests and enhance their knowledge while having fun. By using our app, you agree to abide by the following terms and conditions:</p>
+
+                    <h2>Acceptance of Terms</h2>
+                    <p>By accessing or using the Quizzers app, you agree to these terms and conditions. If you do not agree with any part of these terms, you may not use our services.</p>
+
+                    <h2>User Registration</h2>
+                    <p>To access certain features of the app, such as participating in real quiz contests and withdrawing winnings, users must register an account. Registration requires providing accurate and complete information, including a username, photo, mobile number, and email verification. Additionally, users must complete KYC (Know Your Customer) verification by providing valid identification documents such as Aadhar card, PAN card, voter card, or passport, and bank KYC by adding bank account details or UPI (Unified Payments Interface) information.</p>
+
+
+                    <h2>User Conduct</h2>
+                    <p>Users are solely responsible for their conduct while using the Quizzers app. Prohibited activities include but are not limited to cheating, fraud, harassment, or any illegal behavior. We reserve the right to suspend or terminate accounts found in violation of these terms.</p>
+
+                    <h2>Contests and Prizes</h2>
+                    <p>Quizzers offers users the opportunity to participate in quiz contests of specific categories with entry fees. Contest rules and prize distribution are outlined within the app and are subject to change at our discretion. Users may also participate in practice quizzes without any entry fee. Winnings from contests are subject to applicable taxes and withdrawal regulations.</p>
+
+                    <h2>Payment Terms</h2>
+                    <p>Users can add funds to their Quizzers wallet through various payment methods provided in the app. Withdrawal of winnings is facilitated through the same wallet, subject to minimum withdrawal limits and verification processes.</p>
+
+                    <h2>Privacy Policy</h2>
+                    <p>We are committed to protecting user privacy. Our privacy policy outlines how we collect, use, and disclose personal information. By using Quizzers, you consent to the terms of our privacy policy.</p>
+
+                    <h2>Intellectual Property</h2>
+                    <p>All content provided through the Quizzers app, including quiz questions, quotes, riddles, and facts, may be subject to intellectual property rights. Users may not reproduce, distribute, or modify any content without prior authorization.</p>
+
+                    <h2>Disclaimer of Warranties</h2>
+                    <p> Quizzers provides its services on an "as is" and "as available" basis, without warranties of any kind. We do not guarantee the accuracy, reliability, or completeness of any content provided through the app.</p>
+
+                    <h2>Limitation of Liability</h2>
+                    <p> Quizzers shall not be liable for any direct, indirect, incidental, special, or consequential damages arising out of or in any way connected with the use of our app.</p>
+
+                    <h2>Changes to Terms</h2>
+                    <p>We reserve the right to modify or update these terms and conditions at any time. Users will be notified of any changes, and continued use of the Quizzers app constitutes acceptance of the updated terms.</p>
+                   
+                    <h2>Governing Law</h2>
+                    <p>These terms and conditions shall be governed by and construed in accordance with the laws of [Jurisdiction], without regard to its conflict of laws provisions.</p>
+
+                    <h2>Contact Us</h2>
+                    <p>If you have any questions or concerns regarding these terms and conditions, please contact us at <span className="companyEmail">quizzers14@gmail.com</span>.</p>
+
+                    <p>By using the Quizzers app, you acknowledge that you have read, understood, and agree to be bound by these terms and conditions.</p>
+                    </div>
+            </div>
         </>
     );
 }
