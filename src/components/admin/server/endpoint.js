89ecef3b7a843bfcdd5aea9f1602699ce31750a1
Ajay Mourya <mouryaajay7463@gmai.com>
2024-04-23 13:27:21 +0530

    update url

--- src/components/admin/server/endpoint.js
+++ src/components/admin/server/endpoint.js
@@ -1,3 +1,4 @@
 // const BASE_URL = 'http://localhost:5000/';
-const BASE_URL = 'https://quizzers-api.onrender.com/';
+// const BASE_URL = 'https://quizzers-api.onrender.com/';
+const BASE_URL = 'http://server.quizzers.in:5000/';
 export default BASE_URL;
